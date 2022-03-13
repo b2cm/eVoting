bf816473e0fb97120545db641a654c5f3d7f9c04
majeed@hs-mittweida.de <majeed@hs-mittweida.de>
2022-03-13 10:45:14 +0000

    Replace ecvrf_nonce_generation.js

--- client/src/ecvrf_nonce_generation.js
+++ client/src/ecvrf_nonce_generation.js
@@ -1,75 +1,157 @@
-const EC = require('elliptic').ec
-const SHA256 = require('js-sha256').sha256
-const BigInteger = require('bn.js') 
-const Hmac = require('js-sha256').sha256.hmac
+const EC = require("elliptic").ec;
+const SHA256 = require("js-sha256").sha256;
+const Hmac = require("js-sha256").sha256.hmac;
+const ec = new EC("p256");
+const PRIME = ec.curve.p;
 
-
-const ec = new EC('p256')
-const PRIME = ec.curve.p
-
-
-function _ecvrf_nonce_generation_rfc6979_2 (sk, h_string) {
-    // a. Process m through the hash function H, yielding:
-    const h1 = SHA256.create().update(h_string).digest()
-    
-    sk = _string_to_bytes(sk)
-    // b. set V = 0x01 0x01 0x01 ... 0x01 such that the length of V, in bits, is equal to 8*ceil(hlen/8).
-    let v = Array(32).fill(0x01, 0, 32)
+var q = BigInt("0x" + "4000000000000000000020108A2E0CC0D99F8A5EF");  
+var qlen = 163;
+var rolen = (qlen + 7) >> 3;
+var rlen = rolen * 8;       
  
-    // c. Set: K = 0x00 0x00 0x00 ... 0x00 such that the length of K, in bits, is equal to 8*ceil(hlen/8).
-    let k = Array(32).fill(0x00, 0, 32)
- 
-    // d.  Set: K = HMAC_K(V || 0x00 || int2octets(x) || bits2octets(h1)) where '||' denotes concatenation.
-    k = Hmac.create(k).update(v.concat([0x00], sk, h1)).digest()
- 
-    // e. Set: V = HMAC_K(V)
-    v = Hmac.create(k).update(v).digest()
- 
-    // f. Set:  K = HMAC_K(V || 0x01 || int2octets(x) || bits2octets(h1))
-    k = Hmac.create(k).update(v.concat([0x01], sk, h1)).digest()
- 
-    // g. Set: V = HMAC_K(V)
-    v = Hmac.create(k).update(v).digest()
- 
-    // h. Apply the following algorithm until a proper value is found for k:
-    // 1. Set T to the empty sequence.  The length of T (in bits) is denoted tlen; thus, at that point, tlen = 0.
-    let t = []
-    // 2.
-    while (t.length < PRIME.bitLength()) {
-        v = Hmac.create(k).update(v).digest()
-        t = t.concat(v) 
-        k_int = BigInt(new BigInteger(v))
-        if (k_int < BigInt(PRIME) - 1n) {
-            return k_int
-        }
-        k = SHA256.hmac.create(k).update(v.concat([0x00])).digest()
-        v = SHA256.hmac.create(k).update(v).digest()
-       
-    }
-    
- }
-
-
-
- /**
- * Convert string to bytes
- * @param octet_string  A string
- * @returns An array of number
+/**
+ *
+ * @param {BigInt} n
+ * @returns {Uint8Array}
  */
-  function _string_to_bytes(octet_string) {
-    const A = []
-    for (i = 0; i < octet_string.length; i=i+2) {
-       A.push(parseInt(octet_string.substr(i, 2), 16))
-    }
-    return A
- }
+function bigIntToByteArray(n) {   
+	let s = n.toString(16);
+	let l = s.length;
 
+	if (l % 2 != 0) {
+		s = "0" + s;
+		l++;
+	}
 
-const k = 'b7de5757b28c349da738409dfba70763ace31a6b15be8216991715fbc833e5fa'
-const SK = 'c9afa9d845ba75166b5c215767b1d6934e50c3db36e89b127b8a622b120f6721'
-const PK = '0360fed4ba255a9d31c961eb74c6356d68c049b8923b61fa6ce669622e60f29fb6'
-const alpha = '73616d706c65'
-const nonce = BigInt(new BigInteger(k, 'hex'))
+	let arrLength = l / 2;
+	let arr = new Uint8Array(arrLength);
 
-console.log(_ecvrf_nonce_generation_rfc6979_2(SK, alpha) == nonce)
+	for (let i = 0; i < arrLength; i++) {
+		let byte = Number.parseInt(s.substring(i * 2, i * 2 + 2), 16);
+		// if (byte > 127) {
+		//   byte = -(~byte & 0xFF) - 1;
+		// }
+		arr[i] = byte;
+	}
 
+	return arr;
+}
+
+/**
+ *
+ * @param {BigInt} n
+ * @returns {Uint8Array}
+ */
+function int2octets(n) {        
+	let o = bigIntToByteArray(n);
+	if (o.length > rolen) {
+		return o.slice(o.length - rolen);
+	} else if (o.length < rolen) {
+		return new Uint8Array([...Array(rolen - o.length).fill(0), ...o]);
+	} else {
+		return o;
+	}
+}
+
+/**
+ *
+ * @param {Uint8Array} n
+ * @returns {Uint8Array}
+ */
+function bits2octets(n) {     
+	let z1 = bits2int(n);
+	let z2 = z1 - q;
+
+	return int2octets(z2 < 0 ? z1 : z2);
+}
+
+/**
+ *
+ * @param {Uint8Array} bits
+ * @returns {BigInt}
+ */
+function bits2int(bits) {      
+	let n = BigInt(
+		"0x" +
+			Array.from(bits)
+				.map((n) => n.toString(16).padStart(2, "0"))
+				.join("")
+	);
+	let vlen = bits.length * 8;
+	if (vlen > qlen) {
+		n = n >> BigInt(vlen - qlen);
+	}
+	return n;
+}
+
+function _ecvrf_nonce_generation_rfc6979_2(sk, h_string) {
+	const h1 = new Uint8Array(
+		SHA256.create().update(bigIntToByteArray(h_string)).arrayBuffer()
+	);
+
+	sk = int2octets(sk);
+	console.log('sk',sk)
+
+	let v = new Uint8Array(Array(32).fill(0x01, 0, 32));
+	console.log('v',v)
+
+	let k = new Uint8Array(Array(32).fill(0x00, 0, 32));
+    console.log('k',k)
+
+	let h1_oct = bits2octets(h1).buffer;
+
+	let zero = new Uint8Array([0]);
+	let one = new Uint8Array([1]);
+
+	k = Hmac.create(k)
+		.update(v)
+		.update(zero)
+		.update(sk)
+		.update(h1_oct)
+		.arrayBuffer();
+		console.log('k',Array.from(new Uint8Array(k)).map(s => s.toString(16)))
+	
+	v = Hmac.create(k).update(v).arrayBuffer();
+	console.log('v',Array.from(new Uint8Array(v)).map(s => s.toString(16)))
+
+	k = Hmac.create(k)
+		.update(v)
+		.update(one)
+		.update(sk)
+		.update(h1_oct)
+		.arrayBuffer();
+
+	v = Hmac.create(k).update(v).arrayBuffer();
+
+	let t = new Uint8Array(Array(rolen).fill(0));
+	while (true) {
+		let toff = 0;
+		while (toff < rolen) {
+			v = new Uint8Array(Hmac.create(k).update(v).arrayBuffer());
+			let cc = Math.min(v.length, t.length - toff);
+			for (let i = 0, j = toff; i < cc; i++, j++) {
+				t[j] = v[i];
+			}
+			toff += cc;          ////??
+		}
+
+		k_int = bits2int(t);
+		if (k_int > 0 && k_int < q) {
+			return k_int;
+		}
+		k = SHA256.hmac.create(k).update(v).update(zero).arrayBuffer();
+		console.log('k',Array.from(new Uint8Array(k)).map(s => s.toString(16)))
+		v = SHA256.hmac.create(k).update(v).arrayBuffer();
+	}
+}
+
+const k = "23AF4074C90A02B3FE61D286D5C87F425E6BDD81B";
+const SK = "09A4D6792295A7F730FC3F2B49CBC0F62E862272F";
+const alpha = "73616d706c65";    //????
+// const alpha = "656c706d6173";
+const nonce = BigInt("0x" + k);
+
+console.log(
+	_ecvrf_nonce_generation_rfc6979_2(BigInt("0x" + SK), BigInt("0x" + alpha)) ==
+		nonce
+);
